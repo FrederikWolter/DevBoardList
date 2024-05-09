8de3bd6b0177a461f8eae352560faa0b652086dd
Frederik Wolter <35914049+FrederikWolter@users.noreply.github.com>
2024-05-09 13:35:42 +0200

    undo delete of tabulator demo config

--- assets/script/tables.js
+++ assets/script/tables.js
@@ -10,7 +10,14 @@
 function loadBoardTable(element) {
     // create tabulator object
     return new Tabulator(element, {
+        layout: "fitColumns", // Automatically fit columns to width of container
+        pagination: true, // Enable pagination
+        paginationSize: 5, // Set number of rows per page
         data: getBoards(),                  // set data (see https://tabulator.info/docs/6.2/data#array)
+        columns: [
+            { title: "ID", field: "id" },
+            { title: "Name", field: "name", width: 200 },
+            { title: "Age", field: "age", hozAlign: "left", sorter: "number" },
         ],
     });
 }
