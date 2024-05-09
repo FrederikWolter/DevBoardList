98b8e5b78b6650a5d33fbd392e4b234c474fa750
Frederik Wolter <35914049+FrederikWolter@users.noreply.github.com>
2024-05-09 12:59:36 +0200

    add data.js

---
+++ assets/script/data.js
@@ -0,0 +1,16 @@
+/**
+ * File containing data definition.
+ */
+
+
+// TODO document types https://stackoverflow.com/a/32139970
+/**
+ * Function returning list of boards.
+ * @returns list of boards.
+ */
+function getBoards() {
+    const boards = [
+    ];
+
+    return boards;
+}
