a790225e88fffdb1c686d8f6771480b90a93929f
Frederik Wolter <35914049+FrederikWolter@users.noreply.github.com>
2024-05-09 13:00:34 +0200

    add placeholder const to config.js

--- assets/script/config.js
+++ assets/script/config.js
@@ -3,5 +3,17 @@
  */
 
 // #region tabulator constants
+/**
+ * Text for empty tabulator due to no data present.
+ * @constant {string}
+ * @see [Link](https://tabulator.info/docs/6.2/layout#placeholder)
+ */
+const TBL_PLACEHOLDER = "No Data Available";
 
+/**
+ * Text for empty tabulator due to no matching data.
+ * @constant {string}
+ * @see [Link](https://tabulator.info/docs/6.2/layout#placeholder)
+ */
+const TBL_PLACEHOLDER_F = "No Matching Data"
 // #endregion
