2412393965e48e4a3d0c6c71f235749e7837c8bf
Frederik Wolter <35914049+FrederikWolter@users.noreply.github.com>
2024-06-08 19:59:26 +0200

    added UOMs to data.js

--- assets/script/data.js
+++ assets/script/data.js
@@ -3,7 +3,71 @@
  */
 
 
-// TODO add UOMFactors
+/**
+ * Class representing a unit of measure.
+ */
+class UnitOfMeasure {
+    /**
+     * Creates a unit of measure.
+     * @param {string} name name of UOM.
+     * @param {number} factor factor of UOM relative to base unit.
+     */
+    constructor(name, factor) {
+        this.name = name;
+        this.factor = factor;
+    }
+
+    /**
+     * Convert UOM to string.
+     * @returns {string} name of UOM.
+     */
+    toString() {
+        return this.name;
+    }
+
+    /**
+     * Convert UOM to number.
+     * @returns {number} factor of UOM.
+     */
+    valueOf() {
+        return this.factor;
+    }
+
+    /**
+     * Generate a list of predefined UOMs.
+     * @returns {Object<string, UnitOfMeasure>} list containing UOMs keyed by their names.
+     */
+    static getUOMList() {
+        const data = [
+            // Binary Units
+            { name: "B", factor: 1 },
+            { name: "KB", factor: 1024 },
+            { name: "MB", factor: 1024 ** 2 },
+            { name: "GB", factor: 1024 ** 3 },
+            { name: "TB", factor: 1024 ** 4 },
+            // Frequency Units
+            { name: "Hz", factor: 1 },
+            { name: "KHz", factor: 1000 },
+            { name: "MHz", factor: 1000 ** 2 },
+            { name: "GHz", factor: 1000 ** 3 },
+            { name: "THz", factor: 1000 ** 4 },
+        ]
+
+        return data.reduce((list, item) => {
+            list[item.name] = new UnitOfMeasure(item.name, item.factor);
+            return list;
+        }, {});
+    }
+}
+
+/**
+ * A list of predefined UOMs.
+ * @const {Object<string, UnitOfMeasure>}
+ */
+const UOM = UnitOfMeasure.getUOMList();
+
+
+
 // TODO use UOMFactors globally?
 // TODO document/comment types https://stackoverflow.com/a/32139970
 /**
@@ -23,16 +87,16 @@ function getBoards() {
             docsSrc: "https://example.com/product1/datasheet/datasheet1.pdf",
             chip: "Controller 1",
             cDocsSrc: "https://example.com/product1/chip/datasheet1.pdf",
-            clock: 1*1000**3,
+            clock: 1 * UOM["GHz"],
             coreCnt: 4,
             coreBit: 64,
             voltOp: 3.3,
             voltIn: 5,
             connector: "Type-A",
-            sizeFlash: 1023,
-            sizeFlashFree: 12*2**30,
-            sizeRAM: 8*2**30,
-            sizeEPROM: 4*2**10,
+            sizeFlash: 1023 * UOM["B"],
+            sizeFlashFree: 12 * UOM["GB"],
+            sizeRAM: 8 * UOM["GB"],
+            sizeEPROM: 4 * UOM["KB"],
             pinoutSrc: "https://example.com/product1/pinout/pinout1.png",
             cntGPIO: 20,
             cntDigital: 16,
@@ -58,16 +122,16 @@ function getBoards() {
             docsSrc: "https://example.com/product2/datasheet/datasheet2.pdf",
             chip: "Controller 2",
             cDocsSrc: "https://example.com/product1/chip/datasheet2.pdf",
-            clock: 2*1000**3,
+            clock: 2 * UOM["GHz"],
             coreCnt: 8,
             coreBit: 32,
             voltOp: 5,
             voltIn: 12,
             connector: "Type-B",
             sizeFlash: 766,
-            sizeFlashFree: 24*2**30,
-            sizeRAM: 16*2**30,
-            sizeEPROM: 8*2**10,
+            sizeFlashFree: 24 * UOM["GB"],
+            sizeRAM: 16 * UOM["GB"],
+            sizeEPROM: 8 * UOM["MB"],
             pinoutSrc: "https://example.com/product2/pinout/pinout2.png",
             cntGPIO: 30,
             cntDigital: 24,
